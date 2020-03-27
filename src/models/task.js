5deea2ddc842bc65ed2bbe177a6fa43ef9b580bc
arbexmb <lucasarbexmb@gmail.com>
2020-03-27 12:52:26 -0300

    End of section 13.

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
   description: {
     type: String,
     required: true,
@@ -15,6 +15,20 @@ const Task = mongoose.model('Task', {
     required: true,
     ref: 'User'
   }
+}, {
+  timestamps: true
 })
+
+taskSchema.methods.toJSON = function() {
+  const task = this
+  const taskObject = task.toObject()
+
+  delete taskObject.owner
+
+  return taskObject
+}
+
+
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
